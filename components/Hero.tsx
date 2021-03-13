c9c551d0216dfb3fac2e80777f6b8f8098603457
Sam Svindland <svindland@Sams-MacBook-Pro.local>
2021-03-12 21:07:16 -0800

    redo home page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,32 +1,82 @@
+import { Button, Theme, Typography, makeStyles } from "@material-ui/core";
 import React, { FC } from "react";
 
 import { contact } from "../src/routes";
 
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    background: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${"/images/hero.jpg"})`,
+    width: "100%",
+    height: "80vh",
+    backgroundSize: "cover",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      backgroundPosition: "top",
+    },
+  },
+  text: {
+    width: "100%",
+    maxWidth: 1040,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  body: {
+    width: "50%",
+    color: "#fff",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      padding: 20,
+    },
+  },
+  typo: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
+
 const Hero: FC = () => {
+  const classes = useStyles();
+
   return (
-    <section className="hero">
-      <div className="hero-width">
-        <div className="hero-body">
-          <h2>Frontend Software Developer</h2>
-          <p>
+    <section className={classes.root}>
+      <div className={classes.text}>
+        <div className={classes.body}>
+          <Typography
+            variant="subtitle1"
+            component="h2"
+            className={classes.typo}
+          >
+            Frontend Software Developer
+          </Typography>
+          <Typography variant="body1" component="p" className={classes.typo}>
             I am a software developer with eight plus years of experiance. I
             specialize in frontend developement with frameworks such as React,
             Vue and Flutter.
-          </p>
-          <p>
+          </Typography>
+          <Typography variant="body1" component="p" className={classes.typo}>
             I have developed many professional level applications and you can
             see my work history under the experiance secion of this webiste.
             Also i ahve developed many personal projects as a showcase of my
             skills and passion for developing fast, reliable, and beautiful
             applications.
-          </p>
-          <p>
+          </Typography>
+          <Typography variant="body1" component="p" className={classes.typo}>
             If you would like to work with me, or hire me for a project, reach
             out using the link below.
-          </p>
-          <a className="btn btn-primary" href={contact}>
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            component="a"
+            href={contact}
+          >
             Contact Me
-          </a>
+          </Button>
         </div>
       </div>
     </section>
--- components/Theme.tsx
+++ components/Theme.tsx
@@ -2,7 +2,50 @@ import { CssBaseline, ThemeProvider, createMuiTheme } from "@material-ui/core";
 import React, { FC } from "react";
 
 const theme = createMuiTheme({
-  overrides: {},
+  overrides: {
+    MuiTypography: {
+      h1: {
+        fontFamily: "CascadiaCode",
+        fontSize: "5em",
+      },
+      h2: {
+        fontFamily: "CascadiaCode",
+        fontSize: "4em",
+      },
+      h3: {
+        fontFamily: "CascadiaCode",
+        fontSize: "3.5em",
+      },
+      h4: {
+        fontFamily: "CascadiaCode",
+        fontSize: "3em",
+      },
+      h5: {
+        fontFamily: "CascadiaCode",
+        fontSize: "2.5em",
+      },
+      h6: {
+        fontFamily: "CascadiaCode",
+        fontSize: "2em",
+      },
+      subtitle1: {
+        fontFamily: "CascadiaCode",
+        fontSize: "2em",
+      },
+      subtitle2: {
+        fontFamily: "CascadiaCode",
+        fontSize: "1.5em",
+      },
+      body1: {
+        fontFamily: "CascadiaCode",
+        fontSize: "1.25em",
+      },
+      body2: {
+        fontFamily: "CascadiaCode",
+        fontSize: "1em",
+      },
+    },
+  },
   props: {
     MuiButtonBase: {
       disableRipple: true,
