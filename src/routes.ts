7c2fa1cfd3cad4ebc5bc3b958983f8630834d217
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-03-17 22:35:04 -0400

    fix routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,8 @@
 export const home = '/';
-export const design =
-    process.env.NODE_ENV === 'production' ? 'design.html' : 'design';
-export const coding =
-    process.env.NODE_ENV === 'production' ? 'coding.html' : 'coding';
-export const games =
-    process.env.NODE_ENV === 'production' ? 'games.html' : 'games';
-export const work =
-    process.env.NODE_ENV === 'production' ? 'work.html' : 'work';
-export const contact =
-    process.env.NODE_ENV === 'production' ? 'contact.html' : 'contact';
-export const terms =
-    process.env.NODE_ENV === 'production' ? 'terms.html' : 'terms';
-export const privacy =
-    process.env.NODE_ENV === 'production' ? 'privacy.html' : 'privacy';
+export const design = 'design';
+export const coding = 'coding';
+export const games = 'games';
+export const work = 'work';
+export const contact = 'contact';
+export const terms = 'terms';
+export const privacy = 'privacy';
